949c20a70490d65726afdab3240c01a1970d2f8a
DucNguyen <duc040301@gmail.com>
2022-05-04 22:08:28 +0700

    updated

--- src/middleware/userValidator.ts
+++ src/middleware/userValidator.ts
@@ -1,10 +1,18 @@
-import { check } from "express-validator";
+import { NextFunction,Request,Response} from "express";
+import { check, validationResult } from "express-validator";
 
 exports.userValidator = [
-  check("userName").isEmpty().withMessage("userName is missing !"),
+  check("userName").not().isEmpty().withMessage("userName is missing !"),
   check("password").isEmpty().withMessage("userName is missing !"),
   check("email").isEmpty().withMessage("userName is missing !"),
   check("avatar").isEmpty().withMessage("userName is missing !"),
   check("phoneNumber").isEmpty().withMessage("userName is missing !"),
   check("address").isEmpty().withMessage("userName is missing !"),
 ];
+exports.validate = (req: Request, res: Response, next: NextFunction) =>{
+  const err = validationResult(req).array()
+  if(!err.length){
+    next()
+  }
+  res.status(400).json({error: err[0].msg})
+}
