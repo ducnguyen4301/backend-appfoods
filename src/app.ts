36b9afd0681545c4524bd1d295710e9b46ebf9be
DucNguyen <duc040301@gmail.com>
2022-06-24 22:29:02 +0700

    updated: swagger

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,32 @@
 import { Request, Response } from "express";
 import { env } from "process";
+import swaggerUi from "swagger-ui-express";
 require("dotenv").config();
 require("./db");
+const swaggerDocument = require("./swagger.json");
 const morgan = require("morgan");
 const express = require("express");
 const app = express();
-const PORT = env.PORT;
+const PORT = env.PORT || 8000;
 const userRouter = require("./routers/user");
 
 app.use(express.json());
 app.use(`/api/${env.VERSION}/user`, userRouter);
 app.use(morgan("dev"));
-// app.get('/',(req: Request, res : Response)=>{
-//   res.send('THIS IS BACK END OF APP FOODS')
-// })
+app.get("/", (req: Request, res: Response) => {
+  res.send("THIS IS BACK END OF APP FOODS");
+});
+
+app.use("/api", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// app.use(
+//   "/docs",
+//   swaggerUi.serve,
+//   swaggerUi.setup(undefined, {
+//     swaggerOptions: {
+//       url: "/swagger.json",
+//     },
+//   })
+// );
 app.listen(PORT, () => {
-  console.log("port is running " + PORT)
+  console.log("port is running " + PORT);
 });
