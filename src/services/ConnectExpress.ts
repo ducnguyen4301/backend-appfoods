c327d18accaad31e06d3683f2c456d3a551a59f4
DucNguyen <duc040301@gmail.com>
2022-06-29 20:01:39 +0700

    updated: git ignore,swagger

--- src/services/ConnectExpress.ts
+++ src/services/ConnectExpress.ts
@@ -9,7 +9,7 @@ export default async(app: Application) => {
     app.use(express.json());
     app.use(morgan("dev"));
 
-    app.use("/api", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+    //app.use("/api", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
     app.use(`/api/${env.VERSION}/user`, userRouter);
 
     return app;
